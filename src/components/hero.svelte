8101b4131a35bd06f1664ffbe73d680c822312ef
Raaid <raaid@protonmail.com>
2021-11-30 16:00:27 +0000

    add the default theme, start making skeletons

---
+++ src/components/hero.svelte
@@ -0,0 +1,13 @@
+<main>
+    <h1>The latest news from every angle.</h1>
+    <p>See how a story is covered by every outlet, all in one piece. Sign up for a weekly newsletter and updates on future tools.</p>
+</main>
+
+<style>
+    main {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TopAppBar, {Title, Row, Section} from '@smui/top-app-bar';
+	import Button, { Label } from '@smui/button';
+	import Hero from '../components/hero.svelte';
+</script>
+
+<TopAppBar
+	variant="static"
+	color="primary">
+	<Row>
+		<Section>
+			<Title>Nabu</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<Button href="#latest"><Label>Latest</Label></Button>
+			<Button href="#about"><Label>About</Label></Button>
+			<Button href="#support"><Label>Support</Label></Button>
+			<Button href="#contact"><Label>Contact</Label></Button>
+			<Button href="#signup"><Label>Sign Up</Label></Button>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<Hero/>
+
+<div>latest</div>
+<div>about</div>
+<div>support</div>
+<div>contact</div>
+<div>footer</div>
