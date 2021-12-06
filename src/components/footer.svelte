93db9bcc439b57b7bb192649e10cd85552616e07
Raaid <raaid@protonmail.com>
2021-12-06 12:50:06 +0000

    add super simple footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,3 +1,33 @@
+<script>
+    import TopAppBar, { Row, Section } from '@smui/top-app-bar';
+    import Button, { Label } from '@smui/button';
+</script>
+
 <main>
-    <p>Here is the footer</p>
+    <TopAppBar
+    variant="static"
+    color="secondary"
+    style="height: 97px; display: flex; justify-content: center;">
+    <Row>
+        <Section align="end" toolbar>
+            <Button href="#latest"><Label><span>Latest</span></Label></Button>
+            <Button href="#about"><Label><span>About</span></Label></Button>
+            <Button href="#support"><Label><span>Support</span></Label></Button>
+            <Button href="#contact"><Label><span>Contact</span></Label></Button>
+            <Button href="#signup"><Label><span>Sign Up</span></Label></Button>
+        </Section>
+    </Row>
+    </TopAppBar>
 </main>
+
+<style>
+    span {
+        font-family: 'Catamaran', sans-serif;
+        letter-spacing: -0.02em;
+        font-size: 20px;
+        line-height: 25px;
+        font-style: normal;
+        font-weight: normal;
+        color: black;
+    }
+</style>
