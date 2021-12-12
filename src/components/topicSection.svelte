db15d2e553dd84fdf14b1f2e4be3f2f0bb143d75
Raaid <raaid@protonmail.com>
2021-12-12 14:07:49 +0000

    add circular progress while waiting for latest

--- src/components/topicSection.svelte
+++ src/components/topicSection.svelte
@@ -18,7 +18,7 @@
         <button class="topic-section-header" on:click={toggle} aria-expanded={isOpen}>
             <span class="topic-section-top-topics">
                 {#each topics.slice(0, 3) as item}
-                <span style="color: hsl(0, 0%, {(1-item.weight)*60}%">{item.term}.</span>
+                <span style="color: hsl(0, 0%, {(1-item.weight)*55}%">{item.term}.</span>
                 {/each}
             </span>
             <svg xmlns="http://www.w3.org/2000/svg" class="plus" width="60" height="60" viewBox="0 0 160 160">
@@ -48,6 +48,9 @@
     .topic-section-top-topics {
         color: #D2CFC9;
         flex-grow: 9;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 
     button {
