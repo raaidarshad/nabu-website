ea0b34f7525a172c8a4a7833daabbf4879afdcb4
Raaid <raaid@protonmail.com>
2021-12-14 10:40:16 +0000

    add more styling to the latest page

--- src/components/latest.svelte
+++ src/components/latest.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
     import TopicSection from '../components/topicSection.svelte';
     import CircularProgress from '@smui/circular-progress';
+    import SegmentedButton, { Segment } from '@smui/segmented-button';
+    import { Label } from '@smui/button';
 
     export let isSection: boolean = true;
     export let topicLimit: number = 3;
 
     export async function loadData() {
         const latestData = await (await fetch('/latestData')).json();
+        console.log(latestData);
         return latestData;
     }
+
+    const theData = loadData();
+
+    let rangeChoices = ['12h', '1d', '3d', '1w'];
+    let rangeSelected = '1d';
+    let sortChoices = ['Sources', 'Articles'];
+    let sortSelected = 'Sources';
 </script>
 
 <main>
@@ -17,16 +27,43 @@
             <h1 id="latest" class="small-header">Here's the Latest</h1>
         {:else}
             <h1 id="latest-page-main-section">Here's the Latest</h1>
+
+            <div id="latest-nav">
+                <div id="latest-nav-range">
+                    <p class="latest-nav-text">News from last:</p>
+                    <SegmentedButton segments={rangeChoices} let:segment singleSelect bind:selected={rangeSelected}>
+                        <Segment {segment}>
+                        <Label>{segment}</Label>
+                        </Segment>
+                    </SegmentedButton>
+                </div>
+
+                <div id="latest-nav-last-updated">
+                    <p class="latest-nav-text">Last updated:</p>
+                    {#await theData then latestData}
+                        <p class="latest-nav-text">{latestData.added_at.split(" ")[1].split(".")[0].slice(0, 5) + ' UTC'}</p>
+                    {/await}
+                </div>
+
+                <div id="latest-nav-sort">
+                    <p class="latest-nav-text">Sort by # of:</p>
+                    <SegmentedButton segments={sortChoices} let:segment singleSelect bind:selected={sortSelected}>
+                        <Segment {segment}>
+                        <Label>{segment}</Label>
+                        </Segment>
+                    </SegmentedButton>
+                </div>
+            </div>
         {/if}
 
         <hr/>
-        {#await loadData()}
+        {#await theData}
         <div id="waiting">
             <CircularProgress style="height: 32px; width: 32px;" indeterminate />
         </div>
         {:then latestData}
             {#each latestData.clusters.slice(0, topicLimit) as cluster}
-                <TopicSection topics={cluster.topics} articles={cluster.articles} keywordLimit={3}/>
+                <TopicSection {cluster} keywordLimit={3}/>
             {/each}
         {/await}
     </div>
@@ -39,7 +76,39 @@
         letter-spacing: -0.02em;
         font-weight: 600;
         text-align: center;
-        /* color: #d2cfc9; */
+        margin-bottom: 0;
+    }
+
+    #latest-nav {
+        display: flex;
+        gap: 5%;
+        align-items: flex-start;
+    }
+
+    #latest-nav-range {
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    #latest-nav-last-updated {
+        width: 30%;
+    }
+
+    .latest-nav-text {
+        font-family: 'Roboto Mono', monospace;
+        font-size: 16px;
+        color: grey;
+        text-align: center;
+        margin: 0;
+    }
+
+    #latest-nav-sort {
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
     }
 
     main {
@@ -53,6 +122,7 @@
         display: flex;
         justify-content: center;
         margin-top: 100px;
+        height: 400px;
     }
 
     #latest-content {
@@ -71,6 +141,10 @@
         #latest-page-main-section {
             font-size: 60px;
         }
+
+        .latest-nav-text {
+            font-size: 14px;
+        }
     }
 
     @media screen and (max-width: 661px) {
@@ -84,6 +158,10 @@
 
         #latest-page-main-section {
             font-size: 30px;
+        }
+
+        .latest-nav-text {
+            font-size: 11px;
         }
     }
 </style>
