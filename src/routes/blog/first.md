3a156d54a8b881b16eb8f5694b329934325224ec
Raaid <raaid@protonmail.com>
2022-01-03 15:00:44 +0000

    Add mdsvex (#9)
    * install mdsvex, first pass at updating config
    
    * add and format blog layout

---
+++ src/routes/blog/first.md
@@ -0,0 +1,8 @@
+---
+title: My First Post
+author: Raaid Arshad
+date: '2022-01-03'
+layout: blog
+---
+
+This is my first post!
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,22 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess(),
+		mdsvex({
+			extensions: ['.svx', '.md'],
+			layout: {
+				blog: './src/routes/blog/_layout.svelte'
+			}
+		})
+	],
+
+	extensions: ['.svelte', '.svx', '.md'],
 
 	kit: {
 		adapter: vercel(),
